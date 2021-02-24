7b1ae3d0c9b01a35aabafd99ecff3ffb06a93cfa
saikrishna-centurylink <saikrishna.chaitanya@centurylink.com>
2021-02-24 15:45:15 +0000

    Final Project

--- main.js
+++ main.js
@@ -1,99 +1,264 @@
-/*
-  Import the built-in path module.
-  See https://nodejs.org/api/path.html
-  The path module provides utilities for working with file and directory paths.
-  IAP requires the path module to access local file modules.
-  The path module exports an object.
-  Assign the imported object to variable path.
-*/
+// Import built-in Node.js package path.
 const path = require('path');
 
 /**
- * Import helper function module located in the same directory
- * as this module. IAP requires the path object's join method
- * to unequivocally locate the file module.
+ * Import the ServiceNowConnector class from local Node.js module connector.js
+ *   and assign it to constant ServiceNowConnector.
+ * When importing local modules, IAP requires an absolute file reference.
+ * Built-in module path's join method constructs the absolute filename.
  */
-const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
+const ServiceNowConnector = require(path.join(__dirname, '/connector.js'));
 
-
-/*
-  Import the ip-cidr npm package.
-  See https://www.npmjs.com/package/ip-cidr
-  The ip-cidr package exports a class.
-  Assign the class definition to variable IPCIDR.
-*/
-const IPCIDR = require('ip-cidr');
-
-
-class IpAddress {
-  constructor() {
-    // IAP's global log object is used to output errors, warnings, and other
-    // information to the console, IAP's log files, or a Syslog server.
-    // For more information, consult the Log Class guide on the Itential
-    // Developer Hub https://developer.itential.io/ located
-    // under Documentation -> Developer Guides -> Log Class Guide
-    log.info('Starting the IpAddress product.');
-  }
-
-  
 /**
- * Calculate and return the first host IP address from a CIDR subnet and IPv4-mapped IPv6 address calculated from getIpv4MappedIpv6Address.
- * @param {string} cidrStr - The IPv4 subnet expressed
- *                 in CIDR format.
- * @param {callback} callback - A callback function.
- * @return {Object} (ipv4, ipv6) - An Object with IPv4 address and IPv4-mapped IPv6 address.
+ * Import built-in Node.js package events' EventEmitter class and
+ * assign it to constant EventEmitter. We will create a child class
+ * from this class.
  */
-  getFirstIpAddress(cidrStr, callback) {
+const EventEmitter = require('events').EventEmitter;
 
-  // Initialize return arguments for callback
-  let firstIpAddress = null;
-  let callbackError = null;
-   let ipAddress = {
-      ipv4: null,
-      ipv6: null
-  };
+/**
+ * The ServiceNowAdapter class.
+ *
+ * @summary ServiceNow Change Request Adapter
+ * @description This class contains IAP adapter properties and methods that IAP
+ *   brokers and products can execute. This class inherits the EventEmitter
+ *   class.
+ */
+class ServiceNowAdapter extends EventEmitter {
 
-  // Instantiate an object from the imported class and assign the instance to variable cidr.
-  const cidr = new IPCIDR(cidrStr);
-  // Initialize options for the toArray() method.
-  // We want an offset of one and a limit of one.
-  // This returns an array with a single element, the first host address from the subnet.
-  const options = {
-    from: 1,
-    limit: 1
-  };
+  /**
+   * Here we document the ServiceNowAdapter class' callback. It must follow IAP's
+   *   data-first convention.
+   * @callback ServiceNowAdapter~requestCallback
+   * @param {(object|string)} responseData - The entire REST API response.
+   * @param {error} [errorMessage] - An error thrown by REST API call.
+   */
 
-  // Use the object's isValid() method to verify the passed CIDR.
-  if (!cidr.isValid()) {
-    // If the passed CIDR is invalid, set an error message.
-    callbackError = 'Error: Invalid CIDR passed to getFirstIpAddress.';
-  } else {
-    // If the passed CIDR is valid, call the object's toArray() method.
-    // Notice the destructering assignment syntax to get the value of the first array's element.
-    [firstIpAddress] = cidr.toArray(options);
+  /**
+   * Here we document the adapter properties.
+   * @typedef {object} ServiceNowAdapter~adapterProperties - Adapter
+   *   instance's properties object.
+   * @property {string} url - ServiceNow instance URL.
+   * @property {object} auth - ServiceNow instance credentials.
+   * @property {string} auth.username - Login username.
+   * @property {string} auth.password - Login password.
+   * @property {string} serviceNowTable - The change request table name.
+   */
 
-    // console.log(firstIpAddress)
-  let ipv4 = firstIpAddress;
-//   console.log(ipv4)
-  let ipv6 = getIpv4MappedIpv6Address(ipv4);
-  
-//   console.log(ipv6)
-  ipAddress = {
-      ipv4,
-      ipv6
-  };
-  
-//   console.log(ipAddress);
+  /**
+   * @memberof ServiceNowAdapter
+   * @constructs
+   *
+   * @description Instantiates a new instance of the Itential ServiceNow Adapter.
+   * @param {string} id - Adapter instance's ID.
+   * @param {ServiceNowAdapter~adapterProperties} adapterProperties - Adapter instance's properties object.
+   */
+  constructor(id, adapterProperties) {
+    // Call super or parent class' constructor.
+    super();
+    // Copy arguments' values to object properties.
+    this.id = id;
+    this.props = adapterProperties;
+    // Instantiate an object from the connector.js module and assign it to an object property.
+    this.connector = new ServiceNowConnector({
+      url: this.props.url,
+      username: this.props.auth.username,
+      password: this.props.auth.password,
+      serviceNowTable: this.props.serviceNowTable
+    });
   }
-  
-  
-  // Call the passed callback function.
-  // Node.js convention is to pass error data as the first argument to a callback.
-  // The IAP convention is to pass returned data as the first argument and error
-  // data as the second argument to the callback function.
-  return callback(ipAddress, callbackError);
-}
-  
+
+  /**
+   * @memberof ServiceNowAdapter
+   * @method connect
+   * @summary Connect to ServiceNow
+   * @description Complete a single healthcheck and emit ONLINE or OFFLINE.
+   *   IAP calls this method after instantiating an object from the class.
+   *   There is no need for parameters because all connection details
+   *   were passed to the object's constructor and assigned to object property this.props.
+   */
+  connect() {
+    // As a best practice, Itential recommends isolating the health check action
+    // in its own method.
+    this.healthcheck();
+  }
+
+    /**
+    * @memberof ServiceNowAdapter
+    * @method healthcheck
+    * @summary Check ServiceNow Health
+    * @description Verifies external system is available and healthy.
+    *   Calls method emitOnline if external system is available.
+    *
+    * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+    *   that handles the response.
+    */
+    healthcheck(callback) {
+    this.getRecord((result, error) => {
+    /**
+        * For this lab, complete the if else conditional
+        * statements that check if an error exists
+        * or the instance was hibernating. You must write
+        * the blocks for each branch.
+        */
+    if (error) {
+        /**
+        * Write this block.
+        * If an error was returned, we need to emit OFFLINE.
+        * Log the returned error using IAP's global log object
+        * at an error severity. In the log message, record
+        * this.id so an administrator will know which ServiceNow
+        * adapter instance wrote the log message in case more
+        * than one instance is configured.
+        * If an optional IAP callback function was passed to
+        * healthcheck(), execute it passing the error seen as an argument
+        * for the callback's errorMessage parameter.
+        */
+        this.emitOffline();
+        log.error("Error from " + this.id)
+        // callback(null, error);
+    } else {
+        /**
+        * Write this block.
+        * If no runtime problems were detected, emit ONLINE.
+        * Log an appropriate message using IAP's global log object
+        * at a debug severity.
+        * If an optional IAP callback function was passed to
+        * healthcheck(), execute it passing this function's result
+        * parameter as an argument for the callback function's
+        * responseData parameter.
+        */
+        this.emitOnline();
+        log.debug("Online");
+        // callback(result, null);
+    }
+    });
+    }
+
+  /**
+   * @memberof ServiceNowAdapter
+   * @method emitOffline
+   * @summary Emit OFFLINE
+   * @description Emits an OFFLINE event to IAP indicating the external
+   *   system is not available.
+   */
+  emitOffline() {
+    this.emitStatus('OFFLINE');
+    log.warn('ServiceNow: Instance is unavailable.');
+  }
+
+  /**
+   * @memberof ServiceNowAdapter
+   * @method emitOnline
+   * @summary Emit ONLINE
+   * @description Emits an ONLINE event to IAP indicating external
+   *   system is available.
+   */
+  emitOnline() {
+    this.emitStatus('ONLINE');
+    log.info('ServiceNow: Instance is available.');
+  }
+
+  /**
+   * @memberof ServiceNowAdapter
+   * @method emitStatus
+   * @summary Emit an Event
+   * @description Calls inherited emit method. IAP requires the event
+   *   and an object identifying the adapter instance.
+   *
+   * @param {string} status - The event to emit.
+   */
+  emitStatus(status) {
+    this.emit(status, { id: this.id });
+  }
+
+  /**
+   * @memberof ServiceNowAdapter
+   * @method getRecord
+   * @summary Get ServiceNow Record
+   * @description Retrieves a record from ServiceNow.
+   *
+   * @param {ServiceNowAdapter~requestCallback} callback - The callback that
+   *   handles the response.
+   */
+  getRecord(callback) {
+    /**
+     * Write the body for this function.
+     * The function is a wrapper for this.connector's get() method.
+     * Note how the object was instantiated in the constructor().
+     * get() takes a callback function.
+     */
+
+      this.connector.get((data, error) => {
+            if (error) {
+                // console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
+                callback(null, error);
+            } else {
+                console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
+                if (typeof data == 'object' && 'body' in data) {
+                    const body = JSON.parse(data.body);
+                    const array = body.result
+                    let returnData = []
+                    array.forEach(element => {
+                        const { number: change_ticket_number, active, priority, description, work_start, work_end, sys_id: change_ticket_key } = element
+                        const obj = {
+                        change_ticket_number,
+                        active,
+                        priority,
+                        description,
+                        work_start,
+                        work_end, 
+                        change_ticket_key
+                        }
+                        returnData.push(obj)
+                    });
+                    callback(returnData, null)
+                }
+            }
+        });
+  }
+
+  /**
+   * @memberof ServiceNowAdapter
+   * @method postRecord
+   * @summary Create ServiceNow Record
+   * @description Creates a record in ServiceNow.
+   *
+   * @param {ServiceNowAdapter~requestCallback} callback - The callback that
+   *   handles the response.
+   */
+  postRecord(callback) {
+    /**
+     * Write the body for this function.
+     * The function is a wrapper for this.connector's post() method.
+     * Note how the object was instantiated in the constructor().
+     * post() takes a callback function.
+     */
+
+      this.connector.post((data, error) => {
+            if (error) {
+                // console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
+                 callback(data, error);
+            } else {
+                console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`)
+                if (typeof data == 'object' && 'body' in data) {
+                    const body = JSON.parse(data.body);
+                    const result = body.result;
+                    const { number: change_ticket_number, active, priority, description, work_start, work_end, sys_id: change_ticket_key } = result;
+                    const obj = {
+                    change_ticket_number,
+                    active,
+                    priority,
+                    description,
+                    work_start,
+                    work_end, 
+                    change_ticket_key
+                    };
+                    callback(obj, error);
+                }
+            }
+        });
+  }
 }
 
-module.exports = new IpAddress;
+module.exports = ServiceNowAdapter;
